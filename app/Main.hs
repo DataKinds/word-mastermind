2b400fe88e7eb017900befc2e9d718f4f9446a17
Aearnus <tslimkemann42@gmail.com>
2018-07-30 22:45:24 -0700

    almost game

--- app/Main.hs
+++ app/Main.hs
@@ -12,21 +12,39 @@ mkStdGameState rng =
         GS.letters = [],
         GS.config = GS.GameConfig {
             GS.maxGuesses = 6,
-            GS.wordListPath = "/usr/share/dict/words"
+            GS.wordListPath = "/usr/share/dict/cracklib-small"
         },
         GS.randomGen = rng
     }
 
-makeGuess :: String -> GS.GameState rng -> GS.GameState rng
-makeGuess guess gs = gs
+checkValidity :: String -> GS.GameState rng -> Either String ()
+checkValidity guess gs
+    | length guess > length (GS.targetWord gs) = Left "Your word is too long!"
+    | length guess < length (GS.targetWord gs) = Left "Your word is too short!"
+--    | any $ map () guess = Left "Your word is too short!"
+    | otherwise = Right ()
+
+makeGuess :: String -> GS.GameState rng -> Either String (GS.GameState rng)
+makeGuess guess gs = checkValidity guess gs >> Right (gs { GS.guessedWords = formatGuess guess : GS.guessedWords gs })
     where
         formatGuess :: String -> String
-        formatGuess = map toUpper  
+        formatGuess = map toUpper
+
+gameLoop :: GS.GameState rng -> IO ()
+gameLoop gs = do
+    print gs
+    guess <- getLine
+    let gs' = makeGuess guess gs
+    case gs' of
+        Right gs'' -> gameLoop gs''
+        Left err -> do
+            putStrLn err
+            gameLoop gs
 
 main :: IO ()
 main = do
     rng <- newStdGen
     let gs = mkStdGameState rng
-    guess <- getLine
-    print gs
+    gameLoop gs
+    
     
--- src/GameState.hs
+++ src/GameState.hs
@@ -3,14 +3,15 @@
 module GameState where
 
 import System.Random
+import Data.List
 
 data Letter = Letter {
-    letter :: String,
+    letter :: Char,
     disabled :: Bool
 }
 
 instance Show Letter where
-    show = letter
+    show l = [letter l]
 
 data GameConfig = GameConfig {
     maxGuesses :: Int,
@@ -25,14 +26,21 @@ data GameState rng = GameState {
     randomGen :: (RandomGen rng) => rng
 }
 
+formatGuessedWord :: String -> String -> String
+formatGuessedWord target guess = unwords $ map charAsBlueString guess
+    where
+        charAsBlueString char = concat ["\ESC[34m", [char], "\ESC[0m"]
+        charAsYellowString char = concat ["\ESC[34m", [char], "\ESC[0m"]
+        charAsRedString char = concat ["\ESC[34m", [char], "\ESC[0m"]
+
 instance Show (GameState rg) where
-    show gs = (unlines . concat)
+    show gs = (intercalate "\n" . concat)
         [
             ["Last words in play:"],
-            concat $ replicate ((maxGuesses $ config gs) - (length $ guessedWords gs)) [
+            concat $ replicate (maxGuesses (config gs) - length (guessedWords gs)) [
                 concat $ replicate (length $ targetWord gs) "_ "
             ],
-            guessedWords gs,
+            map (formatGuessedWord (targetWord gs)) (guessedWords gs),
             [""],
             ["Letters in play:"],
             [unwords $ map show (letters gs)],
